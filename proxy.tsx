7a21942465cc2bbcd837a37b717328c53a86cddd
Sergiy Likhnovskiy <slihnovskiy@gmail.com>
2026-08-16 12:38:45 +0300

    fixed proxy private rout

--- proxy.tsx
+++ proxy.tsx
@@ -70,5 +70,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/profile/:path*", "/notes/:path", "/sign-in", "/sign-up"],
+  matcher: ["/profile/:path*", "/notes/:path*", "/sign-in", "/sign-up"],
 };
